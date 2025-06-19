e2aeaa4a3efe138dcb2af8f058b37073ab7e5402
Burak Ozarslan <burakozarslan@icloud.com>
2025-06-19 09:50:05 +0100

    refactor: return object from API endpoint
    - Updated API response to return an object.
    - Removed unnecessary type declarations.
    - Updated client to handle object response.
    - Improved API response structure.
    - Updated client-side display of data.

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
-export default function Home() {
+async function fetchMessage() {
+  const res = await fetch("http://localhost:8000");
+  const data: { message: string } = await res.json();
+  return data;
+}
+
+export default async function Home() {
+  const data = await fetchMessage();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -50,6 +58,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <div>{data?.message}</div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
