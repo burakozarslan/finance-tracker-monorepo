11b566016f22f0c1a4f4f19323d7c7a2dcaa1d73
Burak Ozarslan <burakozarslan@icloud.com>
2025-06-22 15:25:03 +0100

    feat(user): add getUserById method
    - Added new method to get user by ID.
    - Uses prisma.user.findFirstOrThrow.

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -7,9 +7,18 @@ export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getUserByEmail(email: User['email']) {
-    const user = await this.prisma.user.findFirst({
+    const user = await this.prisma.user.findFirstOrThrow({
       where: {
         email,
+      },
+    });
+    return user;
+  }
+
+  async getUserById(id: User['id']) {
+    const user = await this.prisma.user.findFirstOrThrow({
+      where: {
+        id,
       },
     });
     return user;
