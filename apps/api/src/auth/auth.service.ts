c43d8305573a93c797d59dc1cd0ee7bd3fa7a4da
Burak Ozarslan <burakozarslan@icloud.com>
2025-06-23 17:31:20 +0100

    refactor(auth): simplify registration logic

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,11 +1,9 @@
 // auth/auth.service.ts
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { User } from '@prisma/client';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
-import { extractUserFields } from './auth.util';
-import type { LoginDto, RegisterDto } from './auth.dto';
+import type { RegisterDto } from './auth.dto';
+import { hashPassword } from './auth.util';
 
 @Injectable()
 export class AuthService {
@@ -15,31 +13,22 @@ export class AuthService {
     private readonly prisma: PrismaService,
   ) {}
 
-  async signToken(user: { id: User['id'] }) {
-    const payload = { sub: user.id };
-    const token = await this.jwtService.signAsync(payload);
-    return token;
-  }
-
   async registerUserOrThrow(dto: RegisterDto) {
+    const existing = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (existing) throw new BadRequestException('This email is already taken');
+
+    const passwordHash = await hashPassword(dto.password);
     const user = await this.prisma.user.create({
       data: {
         email: dto.email,
-        password: dto.password,
-        name: dto.name,
+        fullName: dto.fullName,
+        passwordHash,
       },
     });
-    const userWithStripeCustomerId =
-      await this.userService.createCustomerIdWithEmail(user.email);
-
-    return extractUserFields(userWithStripeCustomerId);
-  }
-
-  async loginUserOrThrow(dto: LoginDto) {
-    const user = await this.userService.getUserWithEmail(dto.email);
-    if (!user) throw new UnauthorizedException('User not found');
-    if (user.password !== dto.password)
-      throw new UnauthorizedException('Invalid credentials');
-    return extractUserFields(user);
+    return user;
   }
 }
