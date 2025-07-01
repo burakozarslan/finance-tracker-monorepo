588e37b165680c3c478897f3fb2c5f8d8eb3cf38
Burak Ozarslan <burakozarslan@icloud.com>
2025-07-01 21:03:03 +0100

    feat(category): add create category endpoint
    - Added POST /categories/create endpoint
    - Implemented category creation logic
    - Secured endpoint with JWT auth guard
    - Returns success message and created category

--- apps/api/src/category/category.controller.ts
+++ apps/api/src/category/category.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { Request as ExpressRequest } from 'express';
 import { User } from '@prisma/client';
 import { JwtAuthGuard } from 'src/auth/auth.jwt-guard';
+import { CreateCategoryDto } from './category.dto';
 
 @Controller('categories')
 export class CategoryController {
@@ -14,5 +15,21 @@ export class CategoryController {
     const { id: userId } = req.user as User;
     const categories = await this.categoryService.getCategoriesByUserId(userId);
     return categories;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('create')
+  async createCategory(
+    // Omit userId field from input as we obtain it from Express request object
+    @Body() input: Omit<CreateCategoryDto, 'userId'>,
+    @Req() req: ExpressRequest,
+  ) {
+    const { id: userId } = req.user as User;
+    const dto = { ...input, userId };
+    const category = await this.categoryService.createCategory(dto);
+    return {
+      message: 'Category successfully created',
+      category,
+    };
   }
 }
