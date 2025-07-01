04bffc8da47bbef103e676b08b706b87d4b599a5
Burak Ozarslan <burakozarslan@icloud.com>
2025-07-01 18:19:00 +0100

    feat(category): add getCategoriesByUserId method
    - Added new method to retrieve categories.
    - Uses Prisma service to fetch data.
    - Filters categories by userId.
    - Returns an array of categories.

--- apps/api/src/category/category.service.ts
+++ apps/api/src/category/category.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDto } from './category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getCategoriesByUserId(userId: User['id']) {
+    const categories = await this.prismaService.category.findMany({
+      where: {
+        userId,
+      },
+    });
+    return categories;
+  }
+}
