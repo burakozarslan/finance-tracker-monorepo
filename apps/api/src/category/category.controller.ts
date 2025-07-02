037e421a96e08495a1ac46c7a91922495a6560d0
Burak Ozarslan <burakozarslan@icloud.com>
2025-07-02 15:13:51 +0100

    refactor(category): Use User decorator for user ID
    - Remove redundant guard declarations
    - Simplify DTO creation
    - Improve code readability

--- apps/api/src/category/category.controller.ts
+++ apps/api/src/category/category.controller.ts
@@ -9,31 +9,32 @@ import {
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { Request as ExpressRequest } from 'express';
-import { User } from '@prisma/client';
+import { User as PrismaUser } from '@prisma/client';
 import { JwtAuthGuard } from 'src/auth/auth.jwt-guard';
 import { CreateCategoryDto, DeleteCategoryDto } from './category.dto';
+import { User } from 'src/user/user.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get()
-  async getCategoriesByUserId(@Req() req: ExpressRequest) {
-    const { id: userId } = req.user as User;
-    const categories = await this.categoryService.getCategoriesByUserId(userId);
+  async getCategoriesByUserId(
+    @Req() req: ExpressRequest,
+    @User() user: PrismaUser
+  ) {
+    const categories = await this.categoryService.getCategoriesByUserId(user.id);
     return categories;
   }
 
-  @UseGuards(JwtAuthGuard)
   @Post('create')
   async createCategory(
     // Omit userId field from input as we obtain it from Express request object
     @Body() input: Omit<CreateCategoryDto, 'userId'>,
-    @Req() req: ExpressRequest,
+    @User() user: PrismaUser,
   ) {
-    const { id: userId } = req.user as User;
-    const dto = { ...input, userId } as CreateCategoryDto;
+    const dto = { ...input, userId: user.id} as CreateCategoryDto;
     const category = await this.categoryService.createCategory(dto);
     return {
       message: 'Category successfully created',
@@ -41,14 +42,12 @@ export class CategoryController {
     };
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete('delete')
   async deleteCategory(
     @Body() input: Omit<DeleteCategoryDto, 'userId'>,
-    @Req() req: ExpressRequest,
+    @User() user: PrismaUser,
   ) {
-    const { id: userId } = req.user as User;
-    const dto = { ...input, userId } as DeleteCategoryDto;
+    const dto = { ...input, userId: user.id } as DeleteCategoryDto;
     const category = await this.categoryService.deleteCategory(dto);
     return {
       message: `Category has been deleted`,
