56a5b6d27c7d85b4ac1f480d5c6ac4f90b9be5e2
Burak Ozarslan <burakozarslan@icloud.com>
2025-07-02 20:32:24 +0100

    feat(api): add transaction DTO
    - Added CreateTransactionDto
    - Added UpdateTransactionDto
    - Includes data validation
    - Uses class-validator decorators
    - Uses Prisma TransactionType enum

--- apps/api/src/transaction/transaction.dto.ts
+++ apps/api/src/transaction/transaction.dto.ts
@@ -1,5 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsDateString, IsNumber, IsOptional, IsString } from 'class-validator';
+import { TransactionType } from '@prisma/client';
 
-export class CreateTransactionDto {}
+export class CreateTransactionDto {
+  @IsString()
+  categoryId: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  amount: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsDateString()
+  transactionDate: Date;
+
+  @IsString()
+  type: TransactionType;
+}
 
 export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {}
