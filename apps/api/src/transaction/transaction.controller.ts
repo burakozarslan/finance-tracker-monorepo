f0e7df009532ff677f302aa2bd407198b296110a
Burak Ozarslan <burakozarslan@icloud.com>
2025-07-02 20:45:42 +0100

    feat(transaction): add auth & update controller
    - Modified create method to accept user ID

--- apps/api/src/transaction/transaction.controller.ts
+++ apps/api/src/transaction/transaction.controller.ts
@@ -6,22 +6,37 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { UpdateTransactionDto, CreateTransactionDto } from './transaction.dto';
+import { JwtAuthGuard } from 'src/auth/auth.jwt-guard';
+import { User } from 'src/user/user.decorator';
+import type { User as PrismaUser } from '@prisma/client';
 
-@Controller('transaction')
+@UseGuards(JwtAuthGuard)
+@Controller('transactions')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionService.create(createTransactionDto);
+  async create(
+    @Body() createTransactionDto: CreateTransactionDto,
+    @User() user: PrismaUser,
+  ) {
+    const transaction = await this.transactionService.create(
+      user.id,
+      createTransactionDto,
+    );
+    return {
+      message: 'Transaction has been created',
+      transaction,
+    };
   }
 
   @Get()
   findAll() {
-    return this.transactionService.findAll();
+    // return this.transactionService.findAll();
   }
 
   @Get(':id')
